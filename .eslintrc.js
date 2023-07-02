7fde2107bb7376a6757d6346a1f07d7fc90b7d63
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2023-07-02 19:50:33 +0100

    chore: config with pnpm

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,10 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: 'airbnb-base',
-  overrides: [
-  ],
+  extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  rules: {
-  },
+  rules: {},
 };
